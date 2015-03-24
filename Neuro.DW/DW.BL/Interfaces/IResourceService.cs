f7daa4e66a34489fbec498717fc250051c35836c
nikita-fiyalko <nikiti488@gmail.com>
2015-03-24 16:57:23 +0200

    Added interface for resource service

---
+++ Neuro.DW/DW.BL/Interfaces/IResourceService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DW.DAL.Entities;
+
+namespace DW.BL.Interfaces
+{
+    /// <summary>
+    /// Contains functionality for managing ResourceItems
+    /// </summary>
+    public interface IResourceService
+    {
+        ResourceItem GetGesourceItem(string resourceName);
+
+        ResourceItemGroup GetResourceGroup(string resourceGroupName);
+
+        void Add(ResourceItem resourceItem);
+
+        void Update(ResourceItem resourceItem);
+
+        void Remove(ResourceItem resourceItem);
+    }
+}
