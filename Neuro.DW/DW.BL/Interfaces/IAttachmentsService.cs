693be8a9175513096f3594ae86ad77933a3c8562
nikita-fiyalko <nikiti488@gmail.com>
2015-03-25 12:31:49 +0200

    added interface for Attachments Service

---
+++ Neuro.DW/DW.BL/Interfaces/IAttachmentsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DW.DAL.Entities;
+
+namespace DW.BL.Interfaces
+{
+    /// <summary>
+    /// Interface that contains functionality for managing Document Attachments
+    /// </summary>
+    public interface IAttachmentsService
+    {
+        IList<Attachment> GetAllByDocumentId(int documentId);
+
+        void Add(Attachment attachment);
+
+        void Update(Attachment attachment);
+
+        void Remove(Attachment attachment);
+    }
+}
