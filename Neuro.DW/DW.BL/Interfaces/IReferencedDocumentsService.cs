0ed5b111500e4432b53fb47e366e49a24dc2726f
nikita-fiyalko <nikiti488@gmail.com>
2015-03-25 12:26:56 +0200

    Added interface for Referenced Documents Service

---
+++ Neuro.DW/DW.BL/Interfaces/IReferencedDocumentsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DW.DAL.Entities;
+
+namespace DW.BL.Interfaces
+{
+    /// <summary>
+    /// Interface that contains functionality for managing Referenced Documents
+    /// </summary>
+    public interface IReferencedDocumentsService
+    {
+        IList<ReferencedDocument> GetAllByDocumentId(int documentId);
+
+        void Add(ReferencedDocument resourceItem);
+
+        void Update(ReferencedDocument resourceItem);
+
+        void Remove(ReferencedDocument resourceItem);
+    }
+}
